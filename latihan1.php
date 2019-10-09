688d1605a74e020e131c843fab3898c0714b9e4a
anisamaharani <53323537+anisamaharani@users.noreply.github.com>
2019-10-09 09:06:25 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,42 @@
+<?php
+   // $nama = "Anisa Maharani";
+   // $pesan = "hallo, selamat datang $nama";
+
+   // echo $pesan ;
+
+    // OPERATOR 
+    // ARITMATIKA
+    // + - * / %
+  // $a = 10;
+  // $b = 20;
+  // echo $a + $b;
+
+   // penggabung nama
+   //.
+  // $nama_depan = "Anisa";
+  // $nama_belakang = "Maharani";
+  // echo $nama_depan . " ". $nama_belakang;
+
+  // ASSIGNMENT / PENUGASAN
+  // =, +=, -=, *=, /=, %=, .=
+ // $x = "Anisa";
+  //$x .= " ";
+ // $x .= "maharani";
+ // echo $x;
+
+//echo "<br>";
+
+  //$angka = 10;
+  //$angka += 5;
+  //$angka += 3;
+ // echo $angka;
+
+ // PERBANDINGAN / COMPARATION
+ // ==, <=, >=, <, >, !=
+
+ // IDENTITASBN / STRICT COMPARATION
+ // ===, !==
+
+ // LOGIKA / LOGICAL
+ // &&, \\, !
+?>
